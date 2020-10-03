e9a8e2e1b48032bbe45c6ee68037b66666d560b4
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-10-03 12:03:19 +0300

    Add manual marker on the map

--- src/App.js
+++ src/App.js
@@ -1,48 +1,46 @@
-import React, { useState, useEffect, useCallback }  from 'react';
+import React, { useState, useEffect, useCallback } from "react";
 
-import{
+import {
   GoogleMap,
   useLoadScript,
   Marker,
-  InfoWindow
+  InfoWindow,
 } from "@react-google-maps/api";
 import mapStyles from "./mapStyles";
-import {formatRelative} from "date-fns";
+import { formatRelative } from "date-fns";
 // import "@reach/combobox/styles.css"
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
 import "./App.css";
 
 function App() {
-  const [markers, setMarkers] = useState([])
+  const [markers, setMarkers] = useState([]);
 
   const libraries = ["places"];
 
   // map size style
   const mapContainerStyle = {
-    width: '100vw',
-    height: '100vh',
+    width: "100vw",
+    height: "100vh",
   };
-
 
   const center = {
-    lat: 32.770050,
+    lat: 32.77005,
     lng: 35.008688,
   };
-
 
   // disableDefaultUI Btns: Map, Satellite
   // zoomControl Btns: +, -
   const options = {
     styles: mapStyles,
     disableDefaultUI: true,
-    zoomControl:true,
+    zoomControl: true,
   };
 
   //map loading msg
-  const {isLoaded, loadError} = useLoadScript({
+  const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
     libraries,
   });
@@ -50,28 +48,41 @@ function App() {
   if (loadError) return "Error loading maps";
   if (!isLoaded) return "Loading maps";
 
-
-  const element = <FontAwesomeIcon icon={faCoffee}  />
-
+  const element = <FontAwesomeIcon icon={faCoffee} />;
 
   return (
     <div className="App">
-      <h4>Uni-Nav <span>{element}</span></h4>
-      <GoogleMap 
-      mapContainerStyle={mapContainerStyle}
-      zoom={18}
-      center={center}
-      options={options}
-      onClick={(event)=>{
-        setMarkers(current => [...current, {
-          lat: event.latLng.lat(),
-          lng: event.latLng.lng(),
-          time: new Date(),
-        }]);
-      console.log(event);
-      
-      }}
-      ></GoogleMap>
+      <h4>
+        Uni-Nav <span>{element}</span>
+      </h4>
+      <GoogleMap
+        mapContainerStyle={mapContainerStyle}
+        zoom={18}
+        center={center}
+        options={options}
+        onClick={(event) => {
+          setMarkers((current) => [
+            ...current,
+            {
+              lat: event.latLng.lat(),
+              lng: event.latLng.lng(),
+              time: new Date(),
+            },
+          ]);
+          console.log(event);
+        }}
+      >
+
+        {/* Add manual marker on the map */}
+        {markers.map((marker) => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+          />
+        ))}
+
+        
+      </GoogleMap>
     </div>
   );
 }
