a553da4292d29bd2f89f63983852f5663cb43c50
shir0206 <40990488+shir0206@users.noreply.github.com>
2020-10-30 16:45:38 +0200

    Add pois images and display them in InfoPOI

--- src/components/InfoPOI.js
+++ src/components/InfoPOI.js
@@ -25,12 +25,14 @@ export const InfoPOI = (props) => {
               {props.selected.desc}
             </h2>
             <p>{props.selected.desc}</p>
-            <img
-              src="https://www.srugim.co.il/i/wp-content/uploads/2015/06/%D7%90%D7%95%D7%A0%D7%99%D7%91%D7%A8%D7%A1%D7%99%D7%98%D7%AA-%D7%97%D7%99%D7%A4%D7%94-%D7%90%D7%9C%D7%A2%D7%93-%D7%92%D7%A8%D7%A9%D7%92%D7%95%D7%A8%D7%9F-%D7%98%D7%9E%D7%A7%D7%90__w650h331q80.jpg"
-              alt="uni"
-              width="100px"
-              height="100px"
-            ></img>
+            {props.selected.img && (
+              <img
+                src={require("../images/" + props.selected.img + ".jpeg")}
+                alt="uni"
+                width="100px"
+                height="100px"
+              ></img>
+            )}
           </div>
         </InfoWindow>
       ) : (
